d35ee31b7540d4e14affe968015be6dfe3b17c9a
Esteban <atjuan.vina7@gmail.com>
2026-05-06 20:02:23 -0500

    Primer commit, con creación de los proyectos separados para el Back y Front, Back ya con las dependencias del initializr

---
+++ Backend_P2/src/test/java/com/triatletamicroservicios/parcial2initializr/Parcial2initializrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triatletamicroservicios.parcial2initializr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parcial2initializrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
