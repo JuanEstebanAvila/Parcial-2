7cace8dea835eded0e2288de05097b718bd1691f
Esteban <atjuan.vina7@gmail.com>
2026-05-06 20:26:22 -0500

    Paquetes nuevos

--- Backend_P2/src/test/java/co/edu/udistrital/backend_p2/TriatletaApplication.java
+++ Backend_P2/src/test/java/co/edu/udistrital/backend_p2/TriatletaApplication.java
@@ -1,10 +1,10 @@
-package com.triatletamicroservicios.parcial2initializr;
+package co.edu.udistrital.backend_p2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Parcial2initializrApplicationTests {
+class TriatletaApplication {
 
 	@Test
 	void contextLoads() {
---
+++ Backend_P2/src/test/java/co/edu/udistrital/backend_p2/repository/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.udistrital.backend_p2.repository;
+
+/**
+ *
+ * @author User
+ */
+public class NewClass {
+    
+}
